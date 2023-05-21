5b9e2020247b0e62f3eab8686910f9112d515448
Edmundo Ortega <ed.ortega@gmail.com>
2023-05-21 14:00:40 -0700

    fixed type error

--- src/routes/api/families/+server.ts
+++ src/routes/api/families/+server.ts
@@ -1,6 +1,6 @@
 import prisma from "$lib/prisma"
 import type { Family } from "@prisma/client"
-import { json, type RequestHandler } from "@sveltejs/kit"
+import type { RequestHandler } from "@sveltejs/kit"
 
 // GET
 export const GET: RequestHandler = ( async (event) => {
@@ -11,38 +11,3 @@ export const GET: RequestHandler = ( async (event) => {
         families: families
     }), {status: 200})
 }) satisfies RequestHandler
-
-// PATCH
-export const PATCH: RequestHandler = ( async (event) => {
-    const { id, isActive } = await event.request.json()
-
-    console.log(`found id of ${id} and isActive is: ${isActive}`)
-
-    const family: Family = await prisma.family.update({
-        where: { id: Number(id)},
-        data: {
-            isActive: isActive
-        }
-    })
-    //return json({success: true, family})
-    return new Response(JSON.stringify({
-        success: true,
-        family: family
-    }), {
-        status: 200
-    })
-    //return new Response(json("hello"), options)
-
-}) satisfies RequestHandler
-
-export const DELETE: RequestHandler = (async (event) => {
-    const {id} = await event.request.json()
-
-    console.log(`Deleting family: ${id}`)
-
-    const family: Family = await prisma.family.delete({
-        where: { id: Number(id)}
-    })
-
-    return json({success: true, family})
-})
